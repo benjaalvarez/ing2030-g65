af9113eb76ccd399a32ed3a3cf611cc4eb06bbd5
Javier Campos <javier.campos@uc.cl>
2023-06-04 16:52:04 -0400

    rastreador v1.1

--- rastreador.py
+++ rastreador.py
@@ -1,40 +1,64 @@
+# Este script toma una carpeta con fotos de una misma camara trampa y devuelve el valor estimado del ecosistema en esa region.
+# para cubrir un área habria que sumar el estimado de cada camara trampa 
 
 import os
 import cv2 as cv
 from tensorflow import keras as krs
 import numpy as np
+import matplotlib.pyplot as plt
 
-class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck'] ##insertar outputs de la Red Neuronal
+class_names = ['Not Animal-Plane', 'Not Animal-Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Not Animal Ship', 'Not Animal-Truck'] ##insertar output labels de la Red Neuronal
 
 
-# recibe una carpeta con imagenes, cada imagen tiene el formato dd/mm/aa-hh:mm:ss
+# recibe una carpeta con imagenes, cada imagen tiene el formato dd/mm/aa_hh:mm:ss. Asumo que todas las imagenes son de un misma locacion
 read_data = {} #diccionario vacio
 
-directory = '\Folder'
+directory = r"C:\Users\javie\Desktop\Innova\Images"
 id_ = 0
-for filename in os.listdir(directory):
+
+for file_name in os.listdir(directory):
     imagen = {}
-    if filename.endswith('.jpg'):
+    if file_name.endswith('.jpg'):
         #leer imagen
-        img = cv.imread(filename)
+        img = cv.imread(directory+'\\' + file_name)
         img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         imagen['data'] = img
         #extraer metadata
-        meta = filename.split('-')
+        meta = file_name.split('_')
         imagen['meta'] = meta
         read_data[str(id_)] = imagen #agregar al diccionario
-        id_ += 1 #siguiente id
+        id_ += 1 #siguiente id    
+
+
 
 #Analisis
 model = krs.models.load_model('image_classifier_1.model') 
 
-for imagen in read_data:
+for key  in read_data.keys():
+    imagen = read_data[key]
     img = imagen['data']
     prediction = model.predict(np.array([img]) / 255)
     index = np.argmax(prediction)
     imagen['Label'] = class_names[index]
 
+valor = 0
+estimados = dict([
+    ('Dog', 1),
+    ('Frog', 8),
+    ('Cat', 2),
+    ('Deer', 15),
+    ('Bird', 9),
+    ('Horse', 6)
+])
 
+for key in read_data.keys():
+    imagen = read_data[key]
+    label = imagen['Label']
+    if label in estimados.keys():
+        valor += estimados[label]
+    else:
+        valor += 0 #redundante, pero me ayuda a leerlo mejor
 
+print(f"El valor estimado de la biodiversidad en este ecosistema es ${valor}")
 
         
